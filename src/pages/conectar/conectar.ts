36e9ed951be10fb9401b0a69fbea3c27c6485fda
Juan Mora <juan.mora@glgroup.cl>
2017-11-18 23:56:09 -0300

    se agrega sistema de conexion

--- src/pages/conectar/conectar.ts
+++ src/pages/conectar/conectar.ts
@@ -2,43 +2,63 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 import { Paho } from 'ng2-mqtt/mqttws31';
+import * as moment from 'moment';
 
 @Component({
 	selector: 'page-conectar',
 	templateUrl: 'conectar.html'
 })
+
 export class ConectarPage {
 
 	client: any;
 
-	txtServidor: String;
+	txtServidor: string = 'mqtt.clubarduino.cl';
+	txtPuerto: number = 9001;
+	btnConectar: string = "Conectar";
+	txtTopico: string = "Raicerk/Luz";
+	MensajesRecibidos: any = [];
 
 	constructor(public navCtrl: NavController) {
-
-		this.txtServidor = "holiwis";
-		console.log("exito conextando");
-		this.client = new Paho.MQTT.Client('iot.eclipse.org', 80, '');
-		this.onMessage();
-		this.onConnectionLost();
-		this.client.connect({ onSuccess: this.onConnected.bind(this) });
-
 	}
 
-	onConnected() {
+
+	ConectarMQTT(){
+		this.client = new Paho.MQTT.Client(this.txtServidor,this.txtPuerto , '');
+		this.onMessage();
+		this.onConnectionLost();
+		this.client.connect({ 
+			onSuccess: this.onConnected.bind(this) 
+		});
+
+		if(this.btnConectar == "Desconectar"){
+			this.btnConectar = "Conectar";
+		}
+		console.info(this.client);
+	}
+
+	onConnected(element) {
 		console.log("Connected");
-		this.client.subscribe("Raicerk/Luz");
-		this.sendMessage('holiwis');
+		this.client.subscribe(this.txtTopico);
+		this.btnConectar = "Desconectar";
+		this.sendMessage("Aca toy");
 	}
 
 	sendMessage(message: string) {
 		let packet = new Paho.MQTT.Message(message);
-		packet.destinationName = "123456";
+		packet.destinationName = this.txtTopico;
 		this.client.send(packet);
 	}
 
 	onMessage() {
 		this.client.onMessageArrived = (message: Paho.MQTT.Message) => {
-			console.log('Message arrived : ' + message.payloadString);
+			
+			let now = moment();
+			
+			this.MensajesRecibidos.push({
+				fecha: now.format("DD-MM-YYYY H:mm:ss"),
+				mensaje: message.payloadString
+			});
 		};
 	}
 
